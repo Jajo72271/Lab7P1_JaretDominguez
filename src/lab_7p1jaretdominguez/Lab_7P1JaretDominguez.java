ad02d62a453cd6d5dd187808086f27186ba403da
Jajo72271 <jajo.dominguez1@gmail.com>
2025-11-28 13:26:34 -0600

    first commit

---
+++ src/lab_7p1jaretdominguez/Lab_7P1JaretDominguez.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab_7p1jaretdominguez;
+
+/**
+ *
+ * @author JARET DOMINGUEZ
+ */
+public class Lab_7P1JaretDominguez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
